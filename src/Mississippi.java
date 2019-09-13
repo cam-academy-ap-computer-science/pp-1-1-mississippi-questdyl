9b2ef09f80b7ff2fc1c19bdd2fd815670ffcd2a2
questdyl000 <questdyl000@CAM-LAB-23.bgsd.k12.wa.us>
2019-09-13 07:42:32 -0700

    So far M and I are done.

--- src/Mississippi.java
+++ src/Mississippi.java
@@ -1,9 +1,24 @@
 
 public class Mississippi {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void M() {
+		// Spells out M
+		System.out.println("M     M");
+		System.out.println("MM   MM");
+		System.out.println("M M M M");
+		System.out.println("M  M  M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+	}
+	public static void I() {
+		System.out.println("IIIII");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("  I");
+		System.out.println("IIIII");
 	}
 
 }
