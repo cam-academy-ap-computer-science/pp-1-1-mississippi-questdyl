9d24cda735115b4e5939ad31a7f8ec24c3f0d94d
questdyl000 <questdyl000@CAM-LAB-23.bgsd.k12.wa.us>
2019-09-16 07:45:15 -0700

    Done.

--- src/Mississippi.java
+++ src/Mississippi.java
@@ -12,13 +12,43 @@ public class Mississippi {
 		System.out.println("M     M");
 	}
 	public static void I() {
-		System.out.println("IIIII");
-		System.out.println("  I");
-		System.out.println("  I");
-		System.out.println("  I");
-		System.out.println("  I");
-		System.out.println("  I");
-		System.out.println("IIIII");
+		System.out.println(" IIIII");
+		System.out.println("   I");
+		System.out.println("   I");
+		System.out.println("   I");
+		System.out.println("   I");
+		System.out.println("   I");
+		System.out.println(" IIIII");
 	}
-
+	public static void S() {
+		System.out.println(" SSSSS");
+		System.out.println("S     S");
+		System.out.println("S");
+		System.out.println(" SSSSS");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS");
+	}
+	public static void P() {
+		System.out.println("PPPPPP");
+		System.out.println("P     P");
+		System.out.println("P     P");
+		System.out.println("PPPPPP");
+		System.out.println("P");
+		System.out.println("P");
+		System.out.println("P");
+	}
+	public static void main(String[] args) {
+		M();
+		I();
+		S();
+		S();
+		I();
+		S();
+		S();
+		I();
+		P();
+		P();
+		I();
+	}
 }
